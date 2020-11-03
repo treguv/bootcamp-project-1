229ef32a5e1904189ac32b3791e9f346e3c9b67f
melissikiss <melissikiss@gmail.com>
2020-11-02 17:40:17 -0800

    modals fully accesible

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+var recentsModal = $("#recents-modal");
+var favoritesModal = $("#favorites-modal");
+
+$("#recents-btn").on("click", function () {
+    recentsModal.addClass("is-active");
+})
+
+$("#close-recents").on("click", function() {
+    recentsModal.removeClass("is-active");
+})
+
+$("#favorites-btn").on("click", function() {
+    favoritesModal.addClass("is-active");
+})
+
+$("#close-favs").on("click", function() {
+    favoritesModal.removeClass("is-active");
+})
+
+
